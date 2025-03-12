a9e8fcfaefbad17099b180f4ffdf5a6f1654201a
gimmicks <3415884603@qq.com>
2025-03-12 16:09:03 +0800

    SmartMedicalSystem

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+//找不到路由添加
+import router from './router/index.js';
 import App from './App.vue'
+import axios from 'axios';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//找不到路由添加
+import VueRouter from 'vue-router';
 
+Vue.use(axios)
+Vue.use(ElementUI);
+//找不到路由添加
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //找不到路由添加
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import LayOut from '@/components/LayOut.vue'
+// import Login from '@/components/Login.vue'
+Vue.use(VueRouter)
+
+const router=new VueRouter({
+    routes:[
+        // {
+            //path:'/Layout',
+            //name:'layout',
+            //component:LayOut
+        // },
+        // {
+            //path:'/',
+            //name:'Login',
+            //component:Login
+        // },
+    ]
+})
+
+export default router
