aa1c338bd2c76507e29976e47255343600ab42ea
gimmicks <3415884603@qq.com>
2025-03-14 20:14:03 +0800

    完成个人信息显示（特别是健康码）以及部分属于医生的页面

---
+++ src/doctor/DoctorLayOut.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- 头部 -->
+    <DoctorHeaDer />
+    <!-- 主题 -->
+
+    <div style="display: flex">
+      <!-- 主题-左边 -->
+      <DoctorASide />
+      <!-- 主题-右边  flex: 1 自适应布局 -->
+      <router-view style="flex: 1;padding: 30px;"  />
+    </div>
+  </div>
+</template>
+    
+    <!-- js -->
+    <script>
+// 导入组件
+import DoctorHeaDer from "@/doctor/DoctorHeaDer";
+import DoctorASide from "@/doctor/DoctorASide";
+
+export default {
+  name: "DoctorLayOut",
+  //  使用哪些组件
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    DoctorHeaDer,
+    // eslint-disable-next-line vue/no-unused-components
+    DoctorASide,
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import LayOut from '@/components/LayOut.vue'
 import Login from '@/components/Login.vue'
 import DoctorList from '@/doctor/DoctorList.vue'
 import DepartmentList from '@/department/DepartmentList.vue'
+import PersonalInfo from '@/user/PersonalInfo.vue'
+import DoctorLayOut from '@/doctor/DoctorLayOut.vue'
+import VaccineMes from '@/vaccine/VaccineMes.vue'
 Vue.use(VueRouter)
 
 const router=new VueRouter({
@@ -22,6 +25,38 @@ const router=new VueRouter({
                     path:'department',
                     name:'department',
                     component: DepartmentList
+                },
+                {
+                    path:'personalInfo',
+                    name:'personalInfo',
+                    component: PersonalInfo
+                },
+            ]
+        },
+        {
+            path:'/DoctorLayout',
+            name:'doctorlayout',
+            component:DoctorLayOut,
+            children:[
+                {
+                    path:'doctorlist',
+                    name:'doctorlist',
+                    component: DoctorList
+                },
+                {
+                    path:'department',
+                    name:'department',
+                    component: DepartmentList
+                },
+                {
+                    path:'personalInfo',
+                    name:'personalInfo',
+                    component: PersonalInfo
+                },
+                {
+                    path:'vaccineMes',
+                    name:'vaccineMes',
+                    component: VaccineMes
                 },
             ]
         },
