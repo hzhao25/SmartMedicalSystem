57834efabd89c4be8e435a36ae0e09498ed61bd4
gimmicks <3415884603@qq.com>
2025-03-15 20:04:59 +0800

    首页图片

--- src/components/LayOut.vue
+++ src/components/LayOut.vue
@@ -9,6 +9,7 @@
       <ASide />
       <!-- 主题-右边  flex: 1 自适应布局 -->
       <router-view style="flex: 1;padding: 30px;"  />
+      <img src="@/assets/首页图片.gif" alt="首页图片" v-if="showImage" style="margin-top: -100px;" width="100%" height="800px">
     </div>
   </div>
 </template>
@@ -21,6 +22,25 @@ import ASide from "@/components/ASide";
 
 export default {
   name: "LayOut",
+  data(){
+    return{
+      showImage: false,
+    };
+  },
+  computed: {
+    isLayoutPage() {
+      return this.$route.path === '/Layout';
+    }
+  },
+  watch: {
+    isLayoutPage(newValue) {
+      this.showImage = newValue;
+    }
+  },
+  created() {
+    // 初始化时检查当前页面路径
+    this.showImage = this.isLayoutPage;
+  },
   //  使用哪些组件
   components: {
     // eslint-disable-next-line vue/no-unused-components
--- src/doctor/DoctorLayOut.vue
+++ src/doctor/DoctorLayOut.vue
@@ -9,6 +9,7 @@
       <DoctorASide />
       <!-- 主题-右边  flex: 1 自适应布局 -->
       <router-view style="flex: 1;padding: 30px;"  />
+      <img src="@/assets/首页图片.gif" alt="首页图片" v-if="showImage" style="margin-top: -100px;" width="100%" height="800px">
     </div>
   </div>
 </template>
@@ -21,6 +22,27 @@ import DoctorASide from "@/doctor/DoctorASide";
 
 export default {
   name: "DoctorLayOut",
+  data(){
+    return{
+      showImage: false,
+    };
+  },
+  computed: {
+    //判断当前页面是不是DoctorLayout
+    isLayoutPage() {
+      return this.$route.path === '/DoctorLayout';
+    }
+  },
+  //对isLayoutPage进行监听
+  watch: {
+    isLayoutPage(newValue) {
+      this.showImage = newValue;
+    }
+  },
+  created() {
+    // 初始化时检查当前页面路径
+    this.showImage = this.isLayoutPage;
+  },
   //  使用哪些组件
   components: {
     // eslint-disable-next-line vue/no-unused-components
