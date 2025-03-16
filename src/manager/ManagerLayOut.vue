807065c77d6812e824c16d2e7a2144544c236fd6
Lai <2039113968@qq.com>
2025-03-16 22:34:34 +0800

    manager部分

---
+++ src/manager/ManagerLayOut.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <!-- 头部 -->
+      <DoctorHeaDer />
+      <!-- 主题 -->
+  
+      <div style="display: flex">
+        <!-- 主题-左边 -->
+        <DoctorASide />
+        <!-- 主题-右边  flex: 1 自适应布局 -->
+        <router-view style="flex: 1;padding: 30px;"  />
+      </div>
+    </div>
+  </template>
+      
+      <!-- js -->
+      <script>
+  // 导入组件
+  import ManagerHeader from "./ManagerHeader.vue";
+  import ManagerAside from "./ManagerAside.vue";
+  
+  export default {
+    name: "DoctorLayOut",
+    //  使用哪些组件
+    components: {
+      // eslint-disable-next-line vue/no-unused-components
+      ManagerHeader,
+      // eslint-disable-next-line vue/no-unused-components
+      ManagerAside,
+    },
+  };
+  </script>
